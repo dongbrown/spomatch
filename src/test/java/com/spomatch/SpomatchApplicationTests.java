ab47eed3c06f5a34c2beb2d13e115b7651bb1708
Choi jaehun <jaehun306@naver.com>
2024-11-20 17:05:11 +0900

    feat: first commit - SpringBoot 프로젝트 빌드

---
+++ src/test/java/com/spomatch/SpomatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spomatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpomatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
