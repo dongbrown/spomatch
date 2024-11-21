f6f5c1c5f6d197537a970aa57a2898059a42ac02
Choi jaehun <jaehun306@naver.com>
2024-11-21 16:45:00 +0900

    feat : enum class 생성, 초기 memberData 생성

---
+++ src/main/java/com/spomatch/common/enums/MemberStatus.java
@@ -0,0 +1,15 @@
+package com.spomatch.common.enums;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public enum MemberStatus {
+    ACTIVE("활성"),
+    INACTIVE("비활성"),
+    SUSPENDED("정지"),
+    WITHDRAWN("탈퇴");
+
+    private final String description;
+}
