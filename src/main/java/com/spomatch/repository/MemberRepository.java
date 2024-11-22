368edb5a5f63949119404c137fca5d36c21394d0
Choi jaehun <jaehun306@naver.com>
2024-11-22 16:06:51 +0900

    Revert "feat : 로그인 시 LoginId를 통해 검색"
    This reverts commit a1e907b7401c26d06c48af5d010467dbc696b04a.

--- src/main/java/com/spomatch/repository/MemberRepository.java
+++ src/main/java/com/spomatch/repository/MemberRepository.java
@@ -3,8 +3,5 @@ package com.spomatch.repository;
 import com.spomatch.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Optional;
-
 public interface MemberRepository extends JpaRepository<Member, Long> {
-    Optional<Member> findByLoginId(String loginId);
 }
