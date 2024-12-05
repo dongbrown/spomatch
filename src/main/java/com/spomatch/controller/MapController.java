833fb4ddb6dfaa4186b5f23de7aa2db60ab747c6
Choi jaehun <jaehun306@naver.com>
2024-12-05 17:50:15 +0900

    refactor: 메인 페이지 수정, 마크 클릭 수정

--- src/main/java/com/spomatch/controller/MapController.java
+++ src/main/java/com/spomatch/controller/MapController.java
@@ -3,7 +3,6 @@ package com.spomatch.controller;
 import com.spomatch.entity.SportsFacilityProgram;
 import com.spomatch.repository.ProgramRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -18,16 +17,7 @@ public class MapController {
 
     private final ProgramRepository programRepository;
 
-    @Value("${kakao.api.key}")
-    private String kakaoApiKey;
-
-    @GetMapping("/map")
-    public String showMap(Model model) {
-        model.addAttribute("kakaoApiKey", kakaoApiKey);
-        return "/map";
-    }
-
-    @GetMapping("/")
+    @GetMapping("/navermap")
     public String showNaverMap(Model model) {
         return "/navermap";
     }
